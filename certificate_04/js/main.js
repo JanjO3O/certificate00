ffc0883daf5f735d916c8babd099080346b0fcb1
JanjO3O <jinjoo7438@gmail.com>
2026-06-08 15:11:11 +0900

    260608_A-2

--- certificate_04/js/main.js
+++ certificate_04/js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
+    $('header nav ul.depth1 > li').on('mouseenter focusin', function(){
+        $('header nav ul.depth1 > li').removeClass('active')
+        $(this).addClass('active')
+        $('header nav ul.depth1 > li > ul.depth2').stop().slideDown()
+        $('header .h_bg').stop().slideDown()
+    })
+    $('header nav').on('mouseleave', function(){
+        $('header nav ul.depth1 > li').removeClass('active')
+        $('header nav ul.depth1 > li > ul.depth2').stop().slideUp()
+        $('header .h_bg').stop().slideUp()
+    })
+    $('header nav ul.depth1 > li:last-child > ul.depth2 > li:last-child').on('focusout', function(){
+        $('header nav ul.depth1 > li').removeClass('active')
+        $('header nav ul.depth1 > li > ul.depth2').stop().slideUp()
+        $('header .h_bg').stop().slideUp()
+    })
+
+    function slide(){
+        $('.slide ul').animate({left: "-1200px"}, 1000, function(){
+            $('.slide ul').append($('.slide ul li:first-child'))
+            $('.slide ul').css({left: 0})
+        })
+    }
+    setInterval(slide, 3000)
     
+    $('.pop_open').on('click', function(){
+        $('.popup').show()
+    })
+    $('.popup .close button').on('click', function(){
+        $('.popup').hide()
+    })
 })
