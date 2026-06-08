0f50ac9a93c077d4e38203dae3312a49e3d37c29
JanjO3O <jinjoo7438@gmail.com>
2026-06-08 22:48:00 +0900

    260608_C유형

--- Certificate_06/js/main.js
+++ Certificate_06/js/main.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-    console.log('readt')
+    $('header nav > ul.depth1 > li').on('mouseenter focusin', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $(this).addClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2').stop().slideUp()
+        $(this).find("ul.depth2").stop().slideDown()
+    })
+    $('header nav').on('mouseleave', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2').stop().slideUp()
+    })
+    $('header nav > ul.depth1 > li:last-child > ul.depth2 > li:last-child').on('focusout', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2').stop().slideUp()
+    })
+
+    let idx = 0
+    function slide(){
+        if(idx < 2){
+            idx++
+        }else{
+            idx = 0
+        }
+        $('.slide ul li').fadeOut()
+        $('.slide ul li').eq(idx).fadeIn()
+    }
+    setInterval(slide, 3000)
+
+    $('.content .bbs > ul > li').on('click', function(){
+        $('.content .bbs > ul > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('.pop_open').on('click', function(){
+        $('.popup').show()
+    })
+    $('.popup .close button').on('click', function(){
+        $('.popup').hide()
+    })
 })
