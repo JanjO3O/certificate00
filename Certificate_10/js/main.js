5d8cbd7b2bce02a6d229a6e1b3c583399d7833db
JanjO3O <jinjoo7438@gmail.com>
2026-06-09 22:58:32 +0900

    260609_D유형(좌우슬라이드)

--- Certificate_10/js/main.js
+++ Certificate_10/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
+    $('header nav > ul.depth1 > li').on('mouseenter focusin', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $(this).addClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2 , header .menu_bg').stop().fadeIn()
+    })
+    $('header nav > ul.depth1 > li').on('mouseleave', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2 , header .menu_bg').stop().fadeOut()
+    })
+    $('header nav > ul.depth1 > li:last-child > ul.depth2 > li:last-child').on('focusout', function(){
+        $('header nav > ul.depth1 > li').removeClass('active')
+        $('header nav > ul.depth1 > li > ul.depth2 , header .menu_bg').stop().fadeOut()
+    })
+
+    function slide(){
+        $('.cont01 .slide ul').animate({left: "-100%"}, 1000, function(){
+            $('.cont01 .slide ul').append($('.cont01 .slide ul li:first-child'))
+            $('.cont01 .slide ul').css({left:0})
+        })
+    }
+    setInterval(slide, 3000)
     
 })
